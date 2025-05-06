ab1a5c27f5fdf023c492037021566c059d01408c
khusmeno <khusmeno@hotmail.com>
2025-05-06 10:52:25 +0300

    add team support url

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -2,6 +2,7 @@
 const confidentialityText = "For internal use only";
 const productNameInTitle = "MP Buddy"; // Name of the product for the Title
 const teamName = "Microsoft System Center Support team"; // Name of the team for the Footer
+const teamSupportUrl = "https://microsoft.github.io/CSS-SystemCenter/"
 
 // Function to load an XML file and return the parsed XML document
 export function createHeader() {
@@ -18,7 +19,7 @@ export function createHeader() {
 export function createFooter() {
     const footer = document.createElement('footer');
     footer.innerHTML = `
-        <p>${teamName}</p>
+        <p><a href="${teamSupportUrl}">${teamName}</a></p>
     `;
     document.body.appendChild(footer); // Add the footer at the bottom of the body
 }
